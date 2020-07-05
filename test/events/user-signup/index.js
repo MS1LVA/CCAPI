43c108b03479e8fb4bd6667c2509ca2813748a5e
boris <basiimov@protonmail.com>
2020-07-04 20:48:45 -0400

    Update test approach for user-signup event

--- test/events/user-signup/index.js
+++ test/events/user-signup/index.js
@@ -4,42 +4,32 @@ const expect = require('chai').expect,
 	describe = require('mocha').describe,
 	arc = require('@architect/functions'),
 	{ v4: uuid } = require('uuid'),
-	{ sleep } = require('../../../lib/utility'),
 	faker = require('faker');
-
-let events;
-
-async function testEvent(identifier) {
-	await sleep(1500);
-	const params = {
-			KeyConditionExpression: 'identifier = :identifier',
-			ExpressionAttributeValues: {
-				':identifier': identifier
-			}
-		},
-		search = await events.query(params);
-	expect(search).to.be.an('object').have.property('Items').an('array').lengthOf(1);
-	expect(search.Items[0]).to.be.an('object').have.property('identifier').equals(identifier);
-}
 
 describe('events/user-signup', () => {
 	it('should add an event to the events table', async () => {
-		events = (await arc.tables()).events;
-		const identifier = uuid();
-		await new Promise((resolve, reject) => {
-			arc.events.publish({
-				name: 'user-signup',
-				payload: {
-					identifier,
-					email: faker.internet.email(),
-					userId: uuid()
-				}
-			},
-			() => {
-				testEvent(identifier)
-					.then(resolve)
-					.catch(reject);
-			});
+		const { events } = await arc.tables(),
+			identifier = uuid();
+		await arc.events.publish({
+			name: 'user-signup',
+			payload: {
+				identifier,
+				email: faker.internet.email(),
+				userId: uuid()
+			}
 		});
-	}).timeout(45000);
+		let results;
+		while (!results) {
+			const params = {
+					KeyConditionExpression: 'identifier = :identifier',
+					ExpressionAttributeValues: {
+						':identifier': identifier
+					}
+				},
+				search = await events.query(params);
+			if (search.Count) results = search;
+		}
+		expect(results).to.be.an('object').have.property('Items').an('array').lengthOf(1);
+		expect(results.Items[0]).to.be.an('object').have.property('identifier').equals(identifier);
+	}).timeout(10000);
 });
